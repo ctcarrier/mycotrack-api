68a7772795f0b07b7c9e3accfcf5da5ccb702e0e
Gregg Carrier <greggcarrier@gmail.com>
2011-11-06 13:12:20 -0800

    consolidated Response classes in a single file

--- src/main/scala/com/mycotrack/api/response/ErrorResponse.scala
+++
@@ -1,9 +0,0 @@
-package com.mycotrack.api.response
-
-/**
- * @author chris_carrier
- * @version 8/19/11
- */
-
-
-case class ErrorResponse(version: Long, request: String, errors: List[String]) extends Response(version, request)
--- src/main/scala/com/mycotrack/api/response/Response.scala
+++ src/main/scala/com/mycotrack/api/response/Response.scala
@@ -7,3 +7,11 @@ package com.mycotrack.api.response
 
 
 abstract class Response(version: Long, request: String)
+
+case class ErrorResponse(version: Long, request: String, errors: List[String]) extends Response(version, request)
+
+case class SuccessResponse[T](version: Long,
+                              request: String,
+                              size: Long,
+                              requestParams: Option[Map[String, _]],
+                              content: List[T]) extends Response(version, request)
